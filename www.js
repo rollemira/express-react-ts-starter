2f92f56ece7097d07a9412dca3187b1c3a1f5588
Ira Mellor <iramellor@drsintuitivetech.com>
2017-06-09 16:36:52 -0400

    fixing bugs, better testing, faster copies

--- www.js
+++ www.js
@@ -5,9 +5,6 @@ var config = require('config');
 /**
  * Module dependencies.
  */
-//module dependencies
-var env = process.env.NODE_ENV || 'development';
-
 var server = (config.get('server.development')) ?
     require('./build/app') :
     require('./app');
